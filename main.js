bb0b9776614142af51e041a748813f7cc357b032
BryanNilsen <bryannilsen@gmail.com>
2021-02-09 22:15:44 -0600

    updated todo instructions

--- main.js
+++ main.js
@@ -1,60 +1,59 @@
-console.log("Object Fundamentals Review")
+console.log("Object Fundamentals Review");
 
-
-// ! ADD AND COMMIT AFTER EACH SECTION
+// ! ADD AND COMMIT
 
 // sample album object
 const tenAlbum = {
-    title: "Ten",
-    artist: "Pearl Jam",
-    released: "8/27/1991",
-    tracks: [
-        {
-            title: "Once",
-            length: 232
-        },
-        {
-            title: "Even Flow",
-            length: 293
-        },
-        {
-            title: "Alive",
-            length: 341
-        },
-        {
-            title: "Why Go",
-            length: 200
-        },
-        {
-            title: "Black",
-            length: 344
-        },
-        {
-            title: "Jeremy",
-            length: 319
-        },
-        {
-            title: "Oceans",
-            length: 162
-        },
-        {
-            title: "Porch",
-            length: 211
-        },
-        {
-            title: "Garden",
-            length: 299
-        },
-        {
-            title: "Deep",
-            length: 258
-        },
-        {
-            title: "Release",
-            length: 543
-        }
-    ]
-}
+  title: "Ten",
+  artist: "Pearl Jam",
+  released: "8/27/1991",
+  tracks: [
+    {
+      title: "Once",
+      length: 232,
+    },
+    {
+      title: "Even Flow",
+      length: 293,
+    },
+    {
+      title: "Alive",
+      length: 341,
+    },
+    {
+      title: "Why Go",
+      length: 200,
+    },
+    {
+      title: "Black",
+      length: 344,
+    },
+    {
+      title: "Jeremy",
+      length: 319,
+    },
+    {
+      title: "Oceans",
+      length: 162,
+    },
+    {
+      title: "Porch",
+      length: 211,
+    },
+    {
+      title: "Garden",
+      length: 299,
+    },
+    {
+      title: "Deep",
+      length: 258,
+    },
+    {
+      title: "Release",
+      length: 543,
+    },
+  ],
+};
 
 // Dot vs. Bracket Notation - accessing properties
 // TODO: USING DOT NOTATION, access the object properties (artist, title, released) and log the following message to the console using string interpolation: "Pearl Jam's Album Ten was released on 8/27/1991"
@@ -65,7 +64,6 @@ const tenAlbum = {
 // TODO: Iterate the tenAlbum object and log the key/value pairs to the console using a for/in loop
 // example: "title": "Ten"
 
-
 // Accessing iterable properties on objects
 // TODO: Iterate the tracks array and output the title of each song and its song length using a for loop of your choosing. The output should look something like this:
 /*
@@ -74,6 +72,7 @@ const tenAlbum = {
     etc.
 */
 
+// ! ADD AND COMMIT
 
 // OBJECT.keys() / .values() / .entries()
 // TODO: log the keys of the tenAlbum object using Object.keys()
@@ -87,26 +86,29 @@ const tenAlbum = {
 // .values()
 // .entries()
 
-
-
+// ! ADD AND COMMIT
 
 // USING THE DATA FROM .setlists
-import { setlistsFromAPI } from './setlists.js'
-console.log('setlistsFromAPI: ', setlistsFromAPI);
+import { setlistsFromAPI } from "./setlists.js";
+console.log("setlistsFromAPI: ", setlistsFromAPI);
 
-// USING the setlistsFromAPI data, find the event dated 20-08-2018
-// Store that event in a variable named 'event' and log event to the console
+// USING the 'setlistsFromAPI' data, find the event dated 20-08-2018 and store that event object in a variable named 'event'
 
-// USING DOT OR BRACKET NOTATION, log the name of the band, venue, and the event date to the console
+// log the 'event' object to the console
 
+// USING DOT OR BRACKET NOTATION AND STRING INTERPOLATION, create a string which includes the name of the band, the venue, and the date of the event and store it in a new variable clled "bandVenueDate"
+// this string should look something like "Pearl Jam - Wrigley Field - 20-08-2018"
 
-// Find the first set of songs of that event and store it in a variable named 'firstSet'
+// log the 'bandVenueDate' string to the console
+
+// Find the first set of songs of the 'event' object and store it in a variable named 'firstSet'
 
 // USING ANY OF THE WAYS TO LOOP AND ACCESS OBJECT PROPERTIES - iterate the firstSet array and log the name of each song
 
+// ! ADD AND COMMIT
 
 // BONUS IF YOU HAVE TIME...
 // Using only properties of the event object we've previously defined
-// TODO: 1. Insert the band's name into the innerHTML of the element with the id of "header" in index.html
+// TODO: 1. Insert the bandVenueDate string into the innerHTML of the element with the id of "header" in index.html
 
 // TODO: 2. Iterate the first set of songs for that event, create a list element for each song with the song's title and insert that new list element into the innerHTML of the element with the id of "setlist" in index.html
